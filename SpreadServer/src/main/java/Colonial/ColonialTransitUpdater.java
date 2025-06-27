366ec314eb57d6c31933628d2635f775bce9f900
pierceleverett <pierceleverett@gmail.com>
2025-06-27 13:21:42 -0500

    testing refresh

--- SpreadServer/src/main/java/Colonial/ColonialTransitUpdater.java
+++ SpreadServer/src/main/java/Colonial/ColonialTransitUpdater.java
@@ -22,7 +22,7 @@ public class ColonialTransitUpdater {
   public static void updateMissingTransitData() {
     try {
       LocalDate latestDate = getLatestDateAcrossCSVs();
-      LocalDate today = LocalDate.now().minusDays(1);
+      LocalDate today = LocalDate.now();
       List<LocalDate> missingDates = getMissingDates(latestDate, today);
 
       if (missingDates.isEmpty()) {
