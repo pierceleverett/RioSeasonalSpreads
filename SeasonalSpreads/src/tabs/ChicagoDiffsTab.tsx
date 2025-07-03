28ba9adf1c0a1d10978bc6bbe4c1fe65efdcf80d
pierceleverett <pierceleverett@gmail.com>
2025-07-03 11:53:44 -0500

    auto update chi

--- SeasonalSpreads/src/tabs/ChicagoDiffsTab.tsx
+++ SeasonalSpreads/src/tabs/ChicagoDiffsTab.tsx
@@ -1,8 +1,34 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import CsvSpreadChart from "../Utilities/CsvSpreadChart";
 
 const ChicagoDiffsTab: React.FC = () => {
   const [activeTab, setActiveTab] = useState<"91Chi" | "ChiCBOB">("91Chi");
+
+      useEffect(() => {
+        const updateSpreads = async () => {
+          try {
+            const response = await fetch(
+              "https://rioseasonalspreads-production.up.railway.app/updateGC",
+              {
+                method: "POST",
+                headers: {
+                  "Content-Type": "application/json",
+                },
+              }
+            );
+    
+            if (!response.ok) {
+              throw new Error("Failed to update spread data");
+            }
+            console.log("Spread data updated successfully");
+          } catch (error) {
+            console.error("Error updating spread data:", error);
+          }
+        };
+    
+        // Call updateSpreads when component mounts and when commodity changes
+        updateSpreads();
+      }, []); // Add dependencies as needed
 
   return (
     <div>
